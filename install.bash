c225ea8cba2d734e913e8f0c9bf57cc2621cde56
kahunsyo <contact2ichino@gmail.com>
2022-08-07 16:24:12 +0900

    fix: cannot run problem

--- install.bash
+++ install.bash
@@ -1,5 +1,6 @@
 #! /bin/bash
 set -ex
+chmod 555 ejectsda poweronhdd uninstall_auto_eject_sda.bash
 cp -i ejectsda poweronhdd uninstall_auto_eject_sda.bash /usr/local/bin/.
 cp -i auto_eject_sda.service /etc/systemd/system/.
 echo "installed"
