24ae5372dce45cff1d14b3dfde27fb8e1ce11052
kahunsyo <contact2ichino@gmail.com>
2022-08-07 16:14:57 +0900

    fix

--- uninstall_auto_eject_sda.bash
+++ uninstall_auto_eject_sda.bash
@@ -4,6 +4,6 @@ set -ex
 
 systemctl disable auto_eject_sda.service
 rm /usr/local/bin/ejectsda /usr/local/bin/poweronhdd
-rm /etc/sytemd/system/auto_eject_sda.service
+rm /etc/systemd/system/auto_eject_sda.service
 rm /usr/local/bin/uninstall_auto_eject_sda.bash
 echo uninstalled
