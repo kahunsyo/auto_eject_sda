0c2a4a4f07f734a53a51af62c872f169bdbaecc5
kahunsyo <contact2ichino@gmail.com>
2022-08-07 16:26:43 +0900

    fix: systemd service

--- install.bash
+++ install.bash
@@ -3,4 +3,5 @@ set -ex
 chmod 555 ejectsda poweronhdd uninstall_auto_eject_sda.bash
 cp -i ejectsda poweronhdd uninstall_auto_eject_sda.bash /usr/local/bin/.
 cp -i auto_eject_sda.service /etc/systemd/system/.
+systemctl daemon-reload
 echo "installed"
