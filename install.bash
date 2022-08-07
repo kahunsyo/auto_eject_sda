75ab6b16902a78d249613da0afb002701978b698
kahunsyo <contact2ichino@gmail.com>
2022-08-07 15:54:00 +0900

    fix

--- install.bash
+++ install.bash
@@ -1,5 +1,5 @@
 #! /bin/bash
 set -ex
-cp -i ejectsda poweronsda /usr/local/bin/.
+cp -i ejectsda poweronsda uninstall_auto_eject_sda /usr/local/bin/.
 cp -i auto_eject_sda.service /etc/systemd/system/.
 echo "installed"
--- uninstall_auto_eject_sda.bash
+++ uninstall_auto_eject_sda.bash
@@ -5,4 +5,5 @@ set -ex
 systemctl disable auto_eject_sda.service
 rm /usr/local/bin/ejectsda /usr/local/bin/poweronhdd
 rm /etc/sytemd/system/auto_eject_sda.service
-
+rm /usr/local/bin/uninstall_auto_eject_sda.bash
+echo uninstalled
